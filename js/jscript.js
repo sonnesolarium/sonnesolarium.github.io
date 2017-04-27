ab4c7cab4404d915c298aa4c2e53cd2995e53773
Sonnesolarium <info@sonnesolarium.hr>
2017-04-27 15:36:10 +0200

    Initial

---
+++ js/jscript.js
@@ -0,0 +1,6 @@
+﻿$('ul.nav').find('a').click(function () {
+    var $href = $(this).attr('href');
+    var $anchor = $('#' + $href).offset();
+    $('body').animate({ scrollTop: $anchor.top });
+    return false;
+});
