550f984b88e11570ad4d98d8a0f2bc85b96bcae4
Sonnesolarium <info@sonnesolarium.hr>
2017-05-03 15:06:41 +0200

    (sve osim modala)

---
+++ js/kontakt.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function () {
+    $("#kontakt_submit").click(function () {
+        sandKontaktData($("#contact-name").val(), $("#contact-oib").val(), $("#tvrtka").val(), $("#message").val(), $("#contact-email-2").val());
+    });
+});
+
+function sandKontaktData(ime, oibID, subjekt, poruka, mail) {
+    debugger;
+    $.ajax({
+        url: "https://formspree.io/info@sonnesolarium.hr",
+        method: "POST",
+        data: { ime: ime, oib: oibID, tvrtka: subjekt, text: poruka, email: mail },
+        dataType: "json"
+    });
+}
